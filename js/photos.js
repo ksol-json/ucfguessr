ae37bc81cadef1eaff1496320fef9933d6f64edb
Kian Soltani <kiansoltani@Kians-MacBook-Air.local>
2025-03-17 22:53:07 -0400

    added helper tools and separated photos into own file

---
+++ js/photos.js
@@ -0,0 +1,104 @@
+const easyPhotos = [
+    "images/easy/IMG_7534.jpeg", // 1
+    "images/easy/IMG_7322.jpeg", // 2
+    "images/easy/IMG_2747.jpeg", // 3
+    "images/easy/IMG_0671.jpeg", // 4
+    "images/easy/IMG_1117.jpeg", // 5
+    "images/easy/IMG_5544.jpeg", // 6
+    "images/easy/IMG_2291.jpeg", // 7
+    "images/easy/IMG_5556.jpeg", // 8
+    "images/easy/IMG_5567.jpeg", // 9
+    "images/easy/IMG_5545.jpeg", // 10
+    "images/easy/IMG_2102.jpeg", // 11
+    "images/easy/590359509979892670--Nick.jpeg", // 12
+    "images/easy/IMG_2439.jpeg", // 13
+    "images/easy/IMG_5548.jpeg", // 14
+    "images/easy/IMG_5549.jpeg", // 15
+    "images/easy/IMG_5551.jpeg", // 16
+    "images/easy/IMG_5561.jpeg", // 17
+    "images/easy/IMG_5562.jpeg", // 18
+    "images/easy/fireworks--Zach Morris.jpeg", // 19
+    "images/easy/IMG_5572.jpeg", // 20
+    "images/easy/PXL_20211111_221010397--Dipsik Arwan.jpeg", // 21
+    "images/easy/IMG_5576.jpeg", // 22
+    "images/easy/statue launch--Zach Morris.jpeg", // 23
+    "images/easy/IMG_5583.jpeg", // 24
+    "images/easy/IMG_5579.jpeg", // 25
+    "images/easy/IMG_5709.jpeg", // 26
+    "images/easy/IMG_5722.jpeg", // 27
+    "images/easy/IMG_5714.jpeg", // 28
+    "images/easy/IMG_5736.jpeg", // 29
+    "images/easy/IMG_5960.jpeg", // 30
+    "images/easy/PXL_20250205_032438690--Jeremiah N..jpeg", // 31
+    "images/easy/109B7E7B-1A7F.jpeg" // 32
+];
+
+const mediumPhotos = [
+    "images/medium/IMG_4257.jpeg", // 1
+    "images/medium/IMG_7859.jpeg", // 2
+    "images/medium/IMG_9438.jpeg", // 3
+    "images/medium/IMG_3440.jpeg", // 4
+    "images/medium/IMG_1555.jpeg", // 5
+    "images/medium/IMG_4170.jpeg", // 6
+    "images/medium/IMG_5582.jpeg", // 7
+    "images/medium/IMG_5570.jpeg", // 8
+    "images/medium/IMG_5577.jpeg", // 9
+    "images/medium/IMG_5555.jpeg", // 10
+    "images/medium/IMG_5578.jpeg", // 11
+    "images/medium/DDD83CD5-5D99-42FA-9C11-B31E9D6A3760--Olive.jpeg", // 12
+    "images/medium/IMG_5565.jpeg", // 13
+    "images/medium/IMG_5553.jpeg", // 14
+    "images/medium/IMG_5540.jpeg", // 15
+    "images/medium/IMG_5569.jpeg", // 16
+    "images/medium/IMG_5558.jpeg", // 17
+    "images/medium/IMG_5547.jpeg", // 18
+    "images/medium/1000101336--Jeremy Weed.jpeg", // 19
+    "images/medium/IMG_5735.jpeg", // 20
+    "images/medium/UCF_Campus-110--josh.jpeg", // 21
+    "images/medium/IMG_5550.jpeg", // 22
+    "images/medium/20241125_165339--Xavier B..jpeg", // 23
+    "images/medium/IMG_5568.jpeg", // 24
+    "images/medium/IMG_5554.jpeg", // 25
+    "images/medium/IMG_5720.jpeg", // 26
+    "images/medium/IMG_5552.jpeg", // 27
+    "images/medium/IMG_5727.jpeg", // 28
+    "images/medium/IMG_5724.jpeg", // 29
+    "images/medium/IMG_5725.jpeg", // 30
+    "images/medium/IMG_5728.jpeg", // 31
+    "images/medium/IMG_5937.jpeg", // 32
+    "images/medium/IMG_5958.jpeg" // 33
+];
+
+const hardPhotos = [
+    "images/hard/IMG_0425.jpeg", // 1
+    "images/hard/IMG_1770.jpeg", // 2
+    "images/hard/IMG_9488.jpeg", // 3
+    "images/hard/IMG_5283.jpeg", // 4
+    "images/hard/IMG_0128.jpeg", // 5
+    "images/hard/IMG_5451.jpeg", // 6
+    "images/hard/IMG_4822.jpeg", // 7
+    "images/hard/IMG_5560.jpeg", // 8
+    "images/hard/IMG_0139.jpeg", // 9
+    "images/hard/IMG_5563.jpeg", // 10
+    "images/hard/IMG_5580.jpeg", // 11
+    "images/hard/IMG_4440--Josh Padilla.jpeg", // 12
+    "images/hard/IMG_5575.jpeg", // 13
+    "images/hard/IMG_0823.jpeg", // 14
+    "images/hard/IMG_5729.jpeg", // 15
+    "images/hard/IMG_5723.jpeg", // 16
+    "images/hard/IMG_5730.jpeg", // 17
+    "images/hard/IMG_5732.jpeg", // 18
+    "images/hard/IMG_1507--Anonymous.jpeg", // 19
+    "images/hard/IMG_0369.jpeg", // 20
+    "images/hard/1000043199--Alex.jpeg", // 21
+    "images/hard/IMG_5953.jpeg", // 22
+    "images/hard/secretphoto--Emzee.jpeg", // 23
+    "images/hard/IMG_5726.jpeg", // 24
+    "images/hard/IMG_5940.jpeg", // 25
+    "images/hard/IMG_5959.jpeg", // 26
+    "images/hard/06471588-D128--Olive.jpeg", // 27
+    "images/hard/statue--Emzee.jpeg", // 28
+    "images/hard/9BDC2D04-CE8D--Olive.jpeg", // 29
+    "images/hard/IMG_9493--thanks for one month of ucfguessr.jpeg", // 30
+    "images/hard/IMG_3082.jpeg" // 31
+];
--- js/script.js
+++ js/script.js
@@ -1,111 +1,3 @@
-// --------------------
-// Photo Lists
-// --------------------
-const easyPhotos = [
-    "images/easy/IMG_7534.jpeg", // 1
-    "images/easy/IMG_7322.jpeg", // 2
-    "images/easy/IMG_2747.jpeg", // 3
-    "images/easy/IMG_0671.jpeg", // 4
-    "images/easy/IMG_1117.jpeg", // 5
-    "images/easy/IMG_5544.jpeg", // 6
-    "images/easy/IMG_2291.jpeg", // 7
-    "images/easy/IMG_5556.jpeg", // 8
-    "images/easy/IMG_5567.jpeg", // 9
-    "images/easy/IMG_5545.jpeg", // 10
-    "images/easy/IMG_2102.jpeg", // 11
-    "images/easy/590359509979892670--Nick.jpeg", // 12
-    "images/easy/IMG_2439.jpeg", // 13
-    "images/easy/IMG_5548.jpeg", // 14
-    "images/easy/IMG_5549.jpeg", // 15
-    "images/easy/IMG_5551.jpeg", // 16
-    "images/easy/IMG_5561.jpeg", // 17
-    "images/easy/IMG_5562.jpeg", // 18
-    "images/easy/fireworks--Zach Morris.jpeg", // 19
-    "images/easy/IMG_5572.jpeg", // 20
-    "images/easy/PXL_20211111_221010397--Dipsik Arwan.jpeg", // 21
-    "images/easy/IMG_5576.jpeg", // 22
-    "images/easy/statue launch--Zach Morris.jpeg", // 23
-    "images/easy/IMG_5583.jpeg", // 24
-    "images/easy/IMG_5579.jpeg", // 25
-    "images/easy/IMG_5709.jpeg", // 26
-    "images/easy/IMG_5722.jpeg", // 27
-    "images/easy/IMG_5714.jpeg", // 28
-    "images/easy/IMG_5736.jpeg", // 29
-    "images/easy/IMG_5960.jpeg", // 30
-    "images/easy/PXL_20250205_032438690--Jeremiah N..jpeg", // 31
-    "images/easy/109B7E7B-1A7F.jpeg" // 32
-];
-
-const mediumPhotos = [
-    "images/medium/IMG_4257.jpeg", // 1
-    "images/medium/IMG_7859.jpeg", // 2
-    "images/medium/IMG_9438.jpeg", // 3
-    "images/medium/IMG_3440.jpeg", // 4
-    "images/medium/IMG_1555.jpeg", // 5
-    "images/medium/IMG_4170.jpeg", // 6
-    "images/medium/IMG_5582.jpeg", // 7
-    "images/medium/IMG_5570.jpeg", // 8
-    "images/medium/IMG_5577.jpeg", // 9
-    "images/medium/IMG_5555.jpeg", // 10
-    "images/medium/IMG_5578.jpeg", // 11
-    "images/medium/DDD83CD5-5D99-42FA-9C11-B31E9D6A3760--Olive.jpeg", // 12
-    "images/medium/IMG_5565.jpeg", // 13
-    "images/medium/IMG_5553.jpeg", // 14
-    "images/medium/IMG_5540.jpeg", // 15
-    "images/medium/IMG_5569.jpeg", // 16
-    "images/medium/IMG_5558.jpeg", // 17
-    "images/medium/IMG_5547.jpeg", // 18
-    "images/medium/1000101336--Jeremy Weed.jpeg", // 19
-    "images/medium/IMG_5735.jpeg", // 20
-    "images/medium/UCF_Campus-110--josh.jpeg", // 21
-    "images/medium/IMG_5550.jpeg", // 22
-    "images/medium/20241125_165339--Xavier B..jpeg", // 23
-    "images/medium/IMG_5568.jpeg", // 24
-    "images/medium/IMG_5554.jpeg", // 25
-    "images/medium/IMG_5720.jpeg", // 26
-    "images/medium/IMG_5552.jpeg", // 27
-    "images/medium/IMG_5727.jpeg", // 28
-    "images/medium/IMG_5724.jpeg", // 29
-    "images/medium/IMG_5725.jpeg", // 30
-    "images/medium/IMG_5728.jpeg", // 31
-    "images/medium/IMG_5937.jpeg", // 32
-    "images/medium/IMG_5958.jpeg" // 33
-];
-
-const hardPhotos = [
-    "images/hard/IMG_0425.jpeg", // 1
-    "images/hard/IMG_1770.jpeg", // 2
-    "images/hard/IMG_9488.jpeg", // 3
-    "images/hard/IMG_5283.jpeg", // 4
-    "images/hard/IMG_0128.jpeg", // 5
-    "images/hard/IMG_5451.jpeg", // 6
-    "images/hard/IMG_4822.jpeg", // 7
-    "images/hard/IMG_5560.jpeg", // 8
-    "images/hard/IMG_0139.jpeg", // 9
-    "images/hard/IMG_5563.jpeg", // 10
-    "images/hard/IMG_5580.jpeg", // 11
-    "images/hard/IMG_4440--Josh Padilla.jpeg", // 12
-    "images/hard/IMG_5575.jpeg", // 13
-    "images/hard/IMG_0823.jpeg", // 14
-    "images/hard/IMG_5729.jpeg", // 15
-    "images/hard/IMG_5723.jpeg", // 16
-    "images/hard/IMG_5730.jpeg", // 17
-    "images/hard/IMG_5732.jpeg", // 18
-    "images/hard/IMG_1507--Anonymous.jpeg", // 19
-    "images/hard/IMG_0369.jpeg", // 20
-    "images/hard/1000043199--Alex.jpeg", // 21
-    "images/hard/IMG_5953.jpeg", // 22
-    "images/hard/secretphoto--Emzee.jpeg", // 23
-    "images/hard/IMG_5726.jpeg", // 24
-    "images/hard/IMG_5940.jpeg", // 25
-    "images/hard/IMG_5959.jpeg", // 26
-    "images/hard/06471588-D128--Olive.jpeg", // 27
-    "images/hard/statue--Emzee.jpeg", // 28
-    "images/hard/9BDC2D04-CE8D--Olive.jpeg", // 29
-    "images/hard/IMG_9493--thanks for one month of ucfguessr.jpeg", // 30
-    "images/hard/IMG_3082.jpeg" // 31
-];
-
 const isMobile = window.innerWidth <= 768;
 
 function showNotification(message) {
@@ -964,3 +856,59 @@ function selectArchiveDate(date) {
 document.addEventListener('DOMContentLoaded', () => {
     currentPlayingDate = new Date();
 });
+
+// Coverage markers functionality (toggle in html, remove comment)
+let coverageMarkers = [];
+let coverageMarkersVisible = false;
+
+function toggleCoverageMarkers() {
+    if (coverageMarkersVisible) {
+        coverageMarkers.forEach(marker => map.removeLayer(marker));
+        coverageMarkersVisible = false;
+    } else {
+        // Clear existing markers
+        coverageMarkers.forEach(marker => map.removeLayer(marker));
+        coverageMarkers = [];
+        
+        const allPhotos = [...easyPhotos, ...mediumPhotos, ...hardPhotos];
+        let processed = 0;
+        
+        allPhotos.forEach(photoUrl => {
+            const img = new Image();
+            img.crossOrigin = "Anonymous";
+            img.src = photoUrl;
+            
+            img.onload = function() {
+                EXIF.getData(this, function() {
+                    const lat = EXIF.getTag(this, "GPSLatitude");
+                    const latRef = EXIF.getTag(this, "GPSLatitudeRef") || "N";
+                    const lon = EXIF.getTag(this, "GPSLongitude");
+                    const lonRef = EXIF.getTag(this, "GPSLongitudeRef") || "W";
+                    
+                    if (lat && lon) {
+                        const latitude = convertDMSToDD(lat, latRef);
+                        const longitude = convertDMSToDD(lon, lonRef);
+                        
+                        const marker = L.circleMarker([latitude, longitude], {
+                            radius: 5,
+                            fillColor: '#ff4444',
+                            color: '#000',
+                            weight: 1,
+                            opacity: 1,
+                            fillOpacity: 0.8
+                        }).addTo(map);
+                        
+                        coverageMarkers.push(marker);
+                    }
+                    
+                    processed++;
+                    if (processed === allPhotos.length) {
+                        showNotification(`Showing ${coverageMarkers.length} photo locations`);
+                    }
+                });
+            };
+        });
+        
+        coverageMarkersVisible = true;
+    }
+}
